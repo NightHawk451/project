1c5be4d1a45106dc157a0d7ebaaa49a2b036e31d
Micah Pearce <micah.r.pearce@gmail.com>
2018-01-30 22:45:44 -0600

    basic working website, home, about, contact page

--- src/profiles/views.py
+++ src/profiles/views.py
@@ -1,9 +1,31 @@
+import random
+
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.views.generic import TemplateView
 
 # Create your views here.
 
+# class HomePageView(TemplateView):
+# 	def get(self, request, **kwargs):
+# 		context = {}
+# 		return render(request, 'index.html', context)
+
 def home(request):
-	if request.user.is_quthenticated():
-		print(request.user.profile.first_nm)
-	context = {}
-	return render(request,"home.html",context)
+    num = random.randint(0, 100000000)
+    some_list = [num, random.randint(0, 100000000), random.randint(0, 100000000), random.randint(0, 100000000) ]
+    context = {"html_var": True, 
+                "num": num,
+                "some_list":some_list
+                }
+    return render(request, "home.html", context)
+
+def about(request):
+    context = {}
+    return render(request, "about.html", context)
+
+def contact(request):
+    context = {}
+    return render(request, "contact.html", context)
+
+
